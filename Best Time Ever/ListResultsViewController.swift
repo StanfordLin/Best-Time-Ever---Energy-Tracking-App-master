16c81d66a37535608bebd74a84945c1c9411ab10
Stanford Lin <stanlin1999@gmail.com>
2016-08-10 14:20:15 -0700

    Working Persistence of SavedTimeMoodArray

--- Best Time Ever/ListResultsViewController.swift
+++ Best Time Ever/ListResultsViewController.swift
@@ -13,7 +13,7 @@ class ListResultsViewController: UITableViewController {
     var alreadyRan: Bool = false
     var feelsArrayNoZeroes: [Int] = []
     var feels: [Int] = []
-    var savedTimeMoodArray = []
+    var savedTimeMoodArray: [TimeEvent] = []
     
     
     override func viewDidLoad() {
@@ -24,63 +24,61 @@ class ListResultsViewController: UITableViewController {
 //        feels = userDefaults.valueForKey("mood") as! [Int]
         
         feelsArrayNoZeroes = feels.filter() {$0 != 0}
-        
+        print("Saved time array:    \(savedTimeMoodArray) savedTimeMoodArray.count \(savedTimeMoodArray.count), savedTimeFeels: \(savedTimeMoodArray.last!.feels), savedTime: \(savedTimeMoodArray.last!.time)")
 
-        let defaults = NSUserDefaults.standardUserDefaults()
         
-        if let timeEvent = defaults.objectForKey("timeEvent") as? NSData {
-            savedTimeMoodArray = NSKeyedUnarchiver.unarchiveObjectWithData(timeEvent) as! [TimeEvent]
+        
+        //        let defaults = NSUserDefaults.standardUserDefaults()
+//        if let timeEvent = defaults.objectForKey("timeEvent") as? NSData {
+//            savedTimeMoodArray = NSKeyedUnarchiver.unarchiveObjectWithData(timeEvent) as! [TimeEvent]
         }
 
 
         
-    }
-//    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    func tableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
-//        print("savedTimeArray: \(savedTimeMoodArray.last?.time)")
-//        return savedTimeMoodArray.count
-//    }
-//    
-//    // 2
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        
-//        // 1
-//        let cell = tableView.dequeueReusableCellWithIdentifier("listResultsTableViewCell", forIndexPath: indexPath) as! ListResultsTableViewCell
-//        
-//        
-//        
-//        print("indexPath: \(indexPath.row)")
-//        
-//        
-//        let timeEvent = self.savedTimeMoodArray[indexPath.row]
-//        cell.storedFeelNumberLabel.text = "\(timeEvent.feels + 1)"
-//              cell.storedTimeLabel.text = "\(timeEvent.time):00"
-//
-//            cell.storedFeelNumberLabel.backgroundColor = UIColor(red: 114 / 255,
-//                                                                 green: 114 / 255,
-//                                                                 blue: 114 / 255,
-//                                                                 alpha: 1.0)
-//        
-//        
-//        
-////        }
-//        
-//        //        cell.storedTimeLabel.layer.cornerRadius = 8 WHY DOESNT IT WORK
-//        
-//        return cell
-//    }
-
     
-}
+    
+    override func didReceiveMemoryWarning() {
+        didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        print("savedTimeArray: \(savedTimeMoodArray.last?.time)")
+        return savedTimeMoodArray.count
+    }
+    
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // 1
+        let cell = tableView.dequeueReusableCellWithIdentifier("listResultsTableViewCell", forIndexPath: indexPath) as! ListResultsTableViewCell
+        
+        
+        
+        print("indexPath: \(indexPath.row)")
+        
+        
+        let timeEvent = savedTimeMoodArray[indexPath.row]
+        cell.storedFeelNumberLabel.text = "\(timeEvent.feels + 1)"
+              cell.storedTimeLabel.text = "\(timeEvent.time):00"
 
+            cell.storedFeelNumberLabel.backgroundColor = UIColor(red: 114 / 255,
+                                                                 green: 114 / 255,
+                                                                 blue: 114 / 255,
+                                                                 alpha: 1.0)
+        
+        
+        
+//        }
+        
+        //        cell.storedTimeLabel.layer.cornerRadius = 8 WHY DOESNT IT WORK
+        
+        return cell
+    }
+}
 
